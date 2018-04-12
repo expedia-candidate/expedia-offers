65f0a996b0ad4a88acb7d668a96f9b3d49103cff
Expedia Candidate <expedia.candidate@gmail.com>
2018-04-12 23:32:28 +0300

    Add more cities

--- app/Classes/Entities/Cities.php
+++ app/Classes/Entities/Cities.php
@@ -9,6 +9,11 @@ class Cities
 {
     public static function getCities()
     {
-        return ['Los Angeles', 'Las Vegas', 'Miami', 'New York', 'Orlando'];
+        return [
+                'Atlanta', 'Bangkok', 'Boston', 'Cancun', 'Chicago', 'Denver', 'Fort Lauderdale',
+                'Honolulu', 'Istanbul', 'Los Angeles', 'Las Vegas', 'Miami', 'Myrtle Beach',
+                'Nashville', 'New Orleans', 'New York', 'Orlando', 'Paris', 'Phoenix', 'Punta Cana',
+                'San Diego', 'San Francisco', 'Seattle', 'Washington'
+        ];
     }
 }
